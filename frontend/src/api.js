63ee8b72229a0bb34b55bd61fbfc8e78b9af8133
Boddu Venkateswara Rao <venkateshboddu923@gmail.com>
2026-04-29 12:59:23 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks"
+  baseURL: "https://task-manager-app-m9qj.onrender.com/api/tasks"
 });
 
 export default API;
